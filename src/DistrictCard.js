8082eea12cf9ea582b06d279554acc452080731b
Laura Turk <laura.lk.turk@gmail.com>
2017-05-03 11:34:44 -0600

    Create new file for year

--- src/DistrictCard.js
+++ src/DistrictCard.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import DistrictYear from './DistrictYear'
 
-const DistrictCard = ({district}) => {
-  console.log(district)
-  for(let i in district){
-    return(
+const DistrictCard = ({ district, dataSet }) => {
+  console.log(Object.values(dataSet))
+  const districtHere = district
+  const yearArray = Object.values(dataSet).map( (year, index) => {
+    <DistrictYear key={index} year={year}/>
+    // console.log(yearArray)
+  })
+  return (
       <div>
         <div>{district}</div>
-        <div>{district[i]}</div>
+        <div>{yearArray}</div>
       </div>
     )
   }
-
-}
 export default DistrictCard
