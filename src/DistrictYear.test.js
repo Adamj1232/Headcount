600ee3966cd803c330509982eba923b7f6cbc98f
Laura Turk <laura.lk.turk@gmail.com>
2017-05-06 15:23:57 -0600

    Finish District Year tests

--- src/DistrictYear.test.js
+++ src/DistrictYear.test.js
@@ -14,4 +14,22 @@ describe('District Year', () => {
     expect(wrapper.find('div').length).toBe(1)
     expect(wrapper.find('div').text()).toEqual(`${mockYear} : ${mockData}`)
   })
+
+  it('data less than .5 should have a class of red', () => {
+    const mockYear = 2004
+    const mockData = .2
+
+    const wrapper = shallow(<DistrictYear year={mockYear} data={mockData} />)
+
+    expect(wrapper.find('.red')).toBeTruthy()
+  })
+
+  it('data greater than .5 should have a class of green', () => {
+    const mockYear = 2004
+    const mockData = .6
+
+    const wrapper = shallow(<DistrictYear year={mockYear} data={mockData} />)
+
+    expect(wrapper.find('.green')).toBeTruthy()    
+  })
 })
